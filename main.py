f1fe22369210ca62b6f22faa7891a051d6e6a051
Brayden <99772720+KD9YVG@users.noreply.github.com>
2024-09-29 17:46:57 +0000

    Create main.py
    And coded it

---
+++ main.py
@@ -0,0 +1,17 @@
+import base64
+
+def decode_base64_file(input_file):
+  try:
+    with open(input_file, 'r') as file:
+      encoded_data = file_read()
+   
+    decoded_data = base64.b64decode(encoded_data).decode('utf-8)
+
+    print(decoded_data)
+
+  except Exception as e:
+    print(f"An error has occourred: {e}")
+
+if __name__ == "__main__":
+    input_file = input("Path to file: ")
+    decode_base64_file(input_file)
